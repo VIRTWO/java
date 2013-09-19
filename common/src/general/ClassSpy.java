93330a2a4f270e3665fc152c1ae20be9ce88f995
VIRTWO <anuj.prateek@gmail.com>
2013-09-19 14:38:12 -0700

    - Added a Class Spy - Added a Primitive comparator - to compare the primitive fields of two objects - where names are same

---
+++ common/src/general/ClassSpy.java
@@ -0,0 +1,73 @@
+package general;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClassSpy {
+
+	private Class<?> c;
+
+	public ClassSpy(Class<?> c) {
+		this.c = c;
+	}
+
+	public List<Field> getFields() {
+		List<Field> fList = new ArrayList<Field>();
+
+		// does not get the private members
+		Field[] dummy = c.getFields();
+		for (Field f : dummy) {
+			fList.add(f);
+		}
+
+		// get the private members
+		dummy = c.getDeclaredFields();
+		for (Field f : dummy) {
+			if (fList.contains(f) == false) {
+				fList.add(f);
+			}
+		}
+
+		return fList;
+	}
+
+	public List<Method> getMethods() {
+		List<Method> mList = new ArrayList<Method>();
+
+		// does not get the private members
+		Method[] dummy = c.getMethods();
+		for (Method m : dummy) {
+			mList.add(m);
+		}
+
+		// get the private members
+		dummy = c.getDeclaredMethods();
+		for (Method m : dummy) {
+			if (mList.contains(m) == false) {
+				mList.add(m);
+			}
+		}
+
+		return mList;
+	}
+
+	public List<Constructor<?>> getConstructors() {
+		List<Constructor<?>> cList = new ArrayList<Constructor<?>>();
+
+		// gets all
+		Constructor<?>[] dummy = c.getDeclaredConstructors();
+		for (Constructor<?> c : dummy) {
+			cList.add(c);
+		}
+
+		return cList;
+	}
+
+	public Package getPackage() {
+		return c.getPackage();
+	}
+
+}
---
+++ common/src/general/PrimitiveComparator.java
@@ -0,0 +1,138 @@
+package general;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class PrimitiveComparator {
+
+	public static class FieldComparisionResult {
+		public int FIELD_COUNT_DIFFERENCE = 0;
+		public List<Field> LEFT_EXTRA_FIELDS = new ArrayList<Field>();
+		public List<Field> RIGHT_EXTRA_FIELDS = new ArrayList<Field>();
+		public Map<String, String> UNEQUAL_VALUES = new HashMap<String, String>();
+		public List<String> SKIPPED = new ArrayList<String>();
+
+		public void print() {
+			System.out.println("Extra fields in left object:");
+			for (Field f : LEFT_EXTRA_FIELDS) {
+				System.out.println(f.getType() + " - " + f.getName());
+			}
+			System.out.println();
+
+			System.out.println("Extra fields in right object:");
+			for (Field f : RIGHT_EXTRA_FIELDS) {
+				System.out.println(f.getType() + " - " + f.getName());
+			}
+			System.out.println();
+
+			System.out.println("Fields with unequal values:");
+			for (Map.Entry<String, String> e : UNEQUAL_VALUES.entrySet()) {
+				System.out.println(e.getKey() + " - " + e.getValue());
+			}
+			System.out.println();
+
+			System.out.println("Fields skipped while matching values:");
+			for (String s : SKIPPED) {
+				System.out.println(s);
+			}
+			System.out.println();
+		}
+	}
+
+	private Set<Class<?>> supportedTypes = new HashSet<Class<?>>(Arrays.asList(
+			byte.class, short.class, int.class, long.class, float.class,
+			double.class, char.class, boolean.class, String.class, Byte.class,
+			Short.class, Integer.class, Long.class, Float.class, Double.class,
+			Character.class, Boolean.class
+
+	));
+
+	private boolean isSupportedFieldType(Class<?> c) {
+		return supportedTypes.contains(c);
+	}
+
+	public FieldComparisionResult compareFieldValues(Object objLeft,
+			Object objRight) throws IllegalArgumentException,
+			IllegalAccessException {
+		FieldComparisionResult result = new FieldComparisionResult();
+
+		ClassSpy cpLeft = new ClassSpy(objLeft.getClass());
+		ClassSpy cpRight = new ClassSpy(objRight.getClass());
+
+		List<Field> fListLeft = cpLeft.getFields();
+		List<Field> fListRight = cpRight.getFields();
+
+		// compare field size
+		if (fListLeft.size() != fListRight.size()) {
+			result.FIELD_COUNT_DIFFERENCE = fListLeft.size()
+					- fListRight.size();
+		}
+
+		Map<String, Field> fNameMapLeft = new HashMap<String, Field>();
+		Map<String, Field> fNameMapRight = new HashMap<String, Field>();
+		for (Field f : fListLeft) {
+			f.setAccessible(true); // we don't care about modifier
+			fNameMapLeft.put(f.getName(), f);
+		}
+		for (Field f : fListRight) {
+			f.setAccessible(true);
+			fNameMapRight.put(f.getName(), f);
+		}
+
+		// find Left extra fields
+		for (Map.Entry<String, Field> e : fNameMapLeft.entrySet()) {
+			if (fNameMapRight.containsKey(e.getKey())) {
+				// similarly named field exists
+				if (e.getValue().getType()
+						.equals(fNameMapRight.get(e.getKey()).getType())) {
+					// they are same type as well
+					continue;
+				}
+			}
+			// they mismatched, so add it to left list
+			result.LEFT_EXTRA_FIELDS.add(e.getValue());
+		}
+
+		// find Right extra fields
+		for (Map.Entry<String, Field> e : fNameMapRight.entrySet()) {
+			if (fNameMapLeft.containsKey(e.getKey())) {
+				if (e.getValue().getType()
+						.equals(fNameMapLeft.get(e.getKey()).getType())) {
+					// we will match the values of the equal elements here
+					// if they are supported, else we will add them to skip list
+					if (isSupportedFieldType(e.getValue().getType()) == false) {
+						// not supported type
+						result.SKIPPED.add(e.getValue().getName());
+						continue;
+					}
+					// implement the toString in class to support other types or
+					// inject special
+					// value compare function for type
+					String rightValue = e.getValue().get(objRight).toString();
+					String leftValue = fNameMapLeft.get(e.getKey())
+							.get(objLeft).toString();
+					if (leftValue.equals(rightValue) == false) {
+						// values are not same
+						result.UNEQUAL_VALUES.put(objLeft.getClass()
+								.getCanonicalName() + " : " + e.getKey(),
+								leftValue);
+						result.UNEQUAL_VALUES.put(objRight.getClass()
+								.getCanonicalName() + " : " + e.getKey(),
+								rightValue);
+					}
+					continue;
+				}
+			}
+			result.RIGHT_EXTRA_FIELDS.add(e.getValue());
+		}
+
+		return result;
+	}
+
+}
