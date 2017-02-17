207934d593089afd18b842bc3bb2b952a0e2a5d5
Anuj Prateek <ap@virtwo.com>
2017-02-17 14:36:27 -0800

    Added Lomuto partition

--- common/src/algo/sort/QuickSort.java
+++ common/src/algo/sort/QuickSort.java
@@ -6,29 +6,93 @@ import java.util.Random;
 
 public class QuickSort<T> implements ISortingStrategy<T> {
 
-	private final Random random = new Random();
+	public static enum PartitionMode {
+		LOMUTO, HOARE
+	}
+
+	public static interface IPivotSelector {
+		public int selectIndex(List<?> data);
+	}
+
+	private final PartitionMode partitionMode;
+	private final IPivotSelector pivotSelector;
+
+	public QuickSort(PartitionMode partitionMode, IPivotSelector pivotSelector) {
+		this.partitionMode = partitionMode;
+		this.pivotSelector = pivotSelector;
+	}
+
+	public QuickSort(PartitionMode partitionMode) {
+		this(partitionMode, new IPivotSelector() {
+			private final Random random = new Random();
+
+			@Override
+			public int selectIndex(List<?> data) {
+				return random.nextInt(data.size());
+			}
+		});
+	}
+
+	public QuickSort() {
+		this(PartitionMode.LOMUTO);
+	}
 
 	@Override
 	public void sort(List<T> data, Comparator<? super T> comparator) {
 		if (data != null) {
-			sortUsingHoarePartition(data, comparator);
+			switch (partitionMode) {
+			case HOARE:
+				sortUsingHoarePartition(data, comparator);
+				break;
+			case LOMUTO:
+				sortUsingLomutoPartition(data, comparator);
+				break;
+			default:
+				throw new IllegalStateException("Partition mode " + partitionMode + " is not supported.");
+			}
 		}
 		// null is always sorted
 	}
 
-	private void sortUsingHoarePartition(List<T> data, Comparator<? super T> comparator) {
+	private void sortUsingLomutoPartition(List<T> data, Comparator<? super T> comparator) {
 		if (data.size() >= 2) {
-
-			int[] p = hoarePartition(data, comparator);
-
-			sort(data.subList(0, p[0]), comparator);
-
-			sort(data.subList(p[1], data.size()), comparator);
+			int p = lomutoPartition(data, comparator);
+			sortUsingLomutoPartition(data.subList(0, p), comparator);
+			sortUsingLomutoPartition(data.subList(p + 1, data.size()), comparator);
 		}
 	}
 
+	private void sortUsingHoarePartition(List<T> data, Comparator<? super T> comparator) {
+		if (data.size() >= 2) {
+			int[] p = hoarePartition(data, comparator);
+			sortUsingHoarePartition(data.subList(0, p[0]), comparator);
+			sortUsingHoarePartition(data.subList(p[1], data.size()), comparator);
+		}
+	}
+
+	private int lomutoPartition(List<T> data, Comparator<? super T> comparator) {
+		int pivotPosition = -1; // no position
+		int pivotIndex = pivotSelector.selectIndex(data);
+		// move pivot element to end
+		swap(data, pivotIndex, data.size() - 1);
+		T pivot = data.get(data.size() - 1);
+		for (int i = 0; i < data.size() - 1; i++) {
+			if (comparator.compare(data.get(i), pivot) <= 0) {
+				pivotPosition = pivotPosition + 1;
+				swap(data, pivotPosition, i);
+			}
+		}
+		// move pivot to its position
+		// which is after the elements smaller than pivot
+		pivotPosition = pivotPosition + 1;
+		swap(data, pivotPosition, data.size() - 1);
+		// when we create low and high side we will ignore element at this
+		// position
+		return pivotPosition;
+	}
+
 	private int[] hoarePartition(List<T> data, Comparator<? super T> comparator) {
-		T pivot = data.get(getPivotIndex(data));
+		T pivot = data.get(pivotSelector.selectIndex(data));
 		int lowSideIndex = -1;
 		int highSideIndex = data.size();
 		while (true) {
@@ -43,43 +107,48 @@ public class QuickSort<T> implements ISortingStrategy<T> {
 			/*
 			 * when lowSideIndex > highSideIndex : they would have gone one step
 			 * extra. so we should have partitions [0, lowSideIndex - 1] and
-			 * [highSideIndex + 1, data.length].
+			 * [highSideIndex + 1, data.length - 1].
 			 * 
 			 * when lowSideIndex == highSideIndex : [0, lowSideIndex - 1] and
-			 * [highSideIndex + 1, data.length] as this condition is true only
-			 * when they meet at pivot
+			 * [highSideIndex + 1, data.length - 1] as this condition is true
+			 * only when they meet at pivot
 			 * 
 			 * so we can return lowSideIndex - 1, highSideIndex + 1 from here or
 			 * {lowSideIndex, highSideIndex + 1} from here since subList
-			 * excludes toIndex if lowSideIndex == highSideIndex then we will
-			 * basically exclude P from future if lowSideIndex > highSideIndex
-			 * then we will divide at crossing point
+			 * excludes toIndex
 			 * 
-			 * so essentially in case of lowSideIndex > highSideIndex we are
+			 * if lowSideIndex == highSideIndex then we will basically exclude P
+			 * from future iterations
+			 * 
+			 * if lowSideIndex > highSideIndex then we will divide at crossing
+			 * point
+			 * 
+			 * in case of lowSideIndex > highSideIndex we are essentially
 			 * returning (highSideIndex + 1) back.
 			 */
 
 			/*
 			 * if pivot is always 0 then we can always return {highSideIndex +
 			 * 1, highSideIndex + 1} When lowSideIndex == highSideIndex it will
-			 * mean pivot will be included in low side. So low side = [0,
-			 * highSideIndex] high side = [highSideIndex + 1, data.length] so we
-			 * can always return highSideIndex + 1. In java when toIndex and
-			 * fromIndex are equal, sublist returns a empty list and we are
-			 * saved from Array out of bound exception.
+			 * mean pivot will be included in one of the sides. So low side =
+			 * [0, highSideIndex] high side = [highSideIndex + 1, data.length]
+			 * so we can always return highSideIndex + 1.
+			 *
+			 * In above case we may end up returning highSideIndex + 1 ==
+			 * data.size() but subList method that we use in recursion fails
+			 * only for (fromIndex < 0 || toIndex > size || fromIndex > toIndex)
+			 * so we are safe.
 			 *
 			 * if pivot is always last element then we cannot always return
 			 * highSideIndex + 1 as data.length == highSideIndex + 1 it will
 			 * result in low = [0, data.length - 1] and high = [data.length,
 			 * data.length] which will lead us to spin on low side. If we start
 			 * returning just highSideIndex then then we will spin on high side.
-			 * Also we may end up with ArrayOutOfBound exception unlike previous
-			 * case.
 			 */
 
 			/*
-			 * As an alternate we can swap pivot to first place before starting
-			 * partitioning. But returning two indexes seem nicer to me.
+			 * Instead of returning 2 values we can swap pivot to 0 position and
+			 * take advantage of the way subList works. But I like it this way.
 			 */
 
 			if (lowSideIndex >= highSideIndex) {
@@ -94,10 +163,6 @@ public class QuickSort<T> implements ISortingStrategy<T> {
 		T holder = data.get(index1);
 		data.set(index1, data.get(index2));
 		data.set(index2, holder);
-	}
-
-	protected int getPivotIndex(List<T> data) {
-		return random.nextInt(data.size());
 	}
 
 }
--- common/tst/sample/SorterTest.java
+++ common/tst/sample/SorterTest.java
@@ -9,38 +9,36 @@ import algo.sort.Sorter;
 
 public class SorterTest {
 
+	// Create a copy before running any sorting
+	public static Integer[][] TEST_DATA = new Integer[][] { 
+			{ 1, 5, 15, 19, 20, 11, 2, 4, 9, 5, 18, 6, 19, 20, 7 },
+			{ 3, 2, 67, 11, 9, 1, 1, 1, 1 }, 
+			{ 1, 1, 1, 1 }, 
+			{ 7, 7, 7, 6, 6, 6 },
+			{ 5, 15, 19, 20, 11, 2, 4, 9, 5, 18, 6, 19, 20, 7 }, 
+			{ 25, 15 }, { 5 }, 
+			{}, 
+			{ 1, 2, 3, 4, 5 },
+			{ 5, 4, 3, 2, 1 }, 
+			{ 1, 1, 2, 2, 3, 3, 4, 4 }, 
+			{ 4, 4, 3, 3, 2, 2, 1, 2 } 
+		};
+
+	public static void testQuickSort() {
+		for (QuickSort.PartitionMode p : QuickSort.PartitionMode.values()) {
+			ISortingStrategy<Integer> sortingStrategy = new QuickSort<Integer>(p);
+			for (Integer[] testData : TEST_DATA) {
+				List<Integer> data = Arrays.asList(Arrays.copyOf(testData, testData.length));
+				Sorter.sort(data, sortingStrategy);
+				if (!Sorter.isSorted(data)) {
+					throw new RuntimeException("Sorting failed.");
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) {
-		
-		ISortingStrategy<Integer> sortingStrategy = new QuickSort<Integer>();
-		
-		List<Integer> data = Arrays.asList(1, 5, 15, 19, 20, 11, 2, 4, 9, 5, 18, 6, 19, 20, 7);
-		Sorter.sort(data, sortingStrategy);
-		System.out.println(Sorter.isSorted(data));
-
-		data = Arrays.asList(1, 1, 1, 1);
-		Sorter.sort(data, sortingStrategy);
-		System.out.println(Sorter.isSorted(data));
-
-		data = Arrays.asList(5, 5, 5, 6, 6, 6);
-		Sorter.sort(data, sortingStrategy);
-		System.out.println(Sorter.isSorted(data));
-
-		data = Arrays.asList(5, 15, 19, 20, 11, 2, 4, 9, 5, 18, 6, 19, 20, 7);
-		Sorter.sort(data, sortingStrategy);
-		System.out.println(Sorter.isSorted(data));
-		
-		data = Arrays.asList(5, 15);
-		Sorter.sort(data, sortingStrategy);
-		System.out.println(Sorter.isSorted(data));
-
-		data = Arrays.asList(5);
-		Sorter.sort(data, sortingStrategy);
-		System.out.println(Sorter.isSorted(data));
-		
-		data = Arrays.asList();
-		Sorter.sort(data, sortingStrategy);
-		System.out.println(Sorter.isSorted(data));
-		
+		testQuickSort();
 		System.out.println("Done!");
 	}
 
